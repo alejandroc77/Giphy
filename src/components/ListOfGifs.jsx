f31993a472b96711a2487448770c06a7397207ae
Jordy Maza <cabreraalejandro1828@gmail.com>
2022-10-14 09:22:36 -0500

    Nuevos cambios

--- src/components/ListOfGifs.jsx
+++ src/components/ListOfGifs.jsx
@@ -2,7 +2,9 @@ import React from "react";
 import { useState, useEffect } from "react";
 import { Gif } from "./Gif";
 import { getGif } from "../services/getGif";
+
 export const ListOfGifs = ({ params }) => {
+  const [loading, setLoading] = useState(false); //-> por defecto loading es false
   //Es recomendable hacer un console.log(props) para saber que es lo que me esta llegando
   // console.log(params);
   const { keyword } = params;
@@ -10,9 +12,16 @@ export const ListOfGifs = ({ params }) => {
   const [gifs, setGifs] = useState([]);
   //useEfect
   useEffect(() => {
-    getGif({ keyword }).then((gifs) => setGifs(gifs));
-  }, [keyword]); //El searchDefault es un efecto secundario que si no tiene ningun valor eso quiere decir que se va a reenderizar una sola vez
-  //retorno
+    setLoading(true); //-> Antes de hacer la llamada a los gifs cambia el estado a true
+    getGif({ keyword }).then((gifs) => {
+      setGifs(gifs);
+      setLoading(false); // -> Y una vez hecha la carga de los gifs pasa a false
+    });
+  }, [keyword]);
+
+  if (loading) return <i>...Cargando</i>;
+  //El keyword es un efecto secundario que si no tiene ningun valor eso quiere decir que se va a reenderizar una sola vez
+
   return (
     <>
       {gifs.map((allGifs) => (
